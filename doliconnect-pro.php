e2d600c14a5599006e07dfab2e9960d568c3d270
ptibogxiv <support@ptibogxiv.net>
2019-03-11 15:52:58 +0100

    Update doliconnect-pro.php

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -1276,14 +1276,13 @@ echo "</div></div>";
 } elseif ( isset($_GET['info']) && constant("DOLICONNECT_CART_ITEM") > 0 ) {
 
 if ( isset($_GET['info']) && $_POST['info'] == 'validation' && !isset($_GET['pay']) && !isset($_GET['validation']) ) {
-
+$thirdparty=$_POST['thirdparty'];
 $ID = $current_user->ID;
 wp_update_user( array( 'ID' => $ID, 'user_email' => sanitize_email($thirdparty['email'])));
 wp_update_user( array( 'ID' => $ID, 'nickname' => sanitize_user($_POST['user_nicename'])));
 wp_update_user( array( 'ID' => $ID, 'display_name' => ucfirst(strtolower($thirdparty['firstname']))." ".strtoupper($thirdparty['lastname'])));
 wp_update_user( array( 'ID' => $ID, 'first_name' => ucfirst(sanitize_user(strtolower($thirdparty['firstname'])))));
 wp_update_user( array( 'ID' => $ID, 'last_name' => strtoupper(sanitize_user($thirdparty['lastname']))));
-wp_update_user( array( 'ID' => $ID, 'description' => sanitize_textarea_field($_POST['description'])));
 wp_update_user( array( 'ID' => $ID, 'user_url' => sanitize_textarea_field($thirdparty['url'])));
 update_user_meta( $ID, 'civility_id', sanitize_text_field($thirdparty['civility_id']));
 update_user_meta( $ID, 'billing_type', sanitize_text_field($thirdparty['morphy']));
