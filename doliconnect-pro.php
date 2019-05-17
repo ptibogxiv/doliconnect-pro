e054175948208aed2192f71adb5838246cd746f6
ptibogxiv <support@ptibogxiv.net>
2019-05-17 14:59:53 +0200

    prepare 3.6.4

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: Doliconnect PRO
  * Plugin URI: https://www.ptibogxiv.net
  * Description: Premium Enhancement of Doliconnect
- * Version: 3.6.3
+ * Version: 3.6.4
  * Author: ptibogxiv
  * Author URI: https://www.ptibogxiv.net/en
  * Network: true
