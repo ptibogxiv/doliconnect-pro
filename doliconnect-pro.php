1a80eabf4dd77bb68953b476a8d20efbae3d19db
ptibogxiv <support@ptibogxiv.net>
2019-08-14 13:07:11 +0200

    Update doliconnect-pro.php

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -148,10 +148,10 @@ print "</div>";
 print "<div class='col-4 col-sm-3 col-md-2 btn-group-vertical' role='group'>";
 if ( !empty($method->default_source) ) { 
 print "<button class='btn btn-light' title='".__( 'Favorite', 'doliconnect-pro' )."' disabled><i class='fas fa-star fa-1x fa-fw' style='color:Gold'></i></button>";
-} elseif ( current_time( 'timestamp', 1) < strtotime($method->expiration.'/1') ) {
-print "<button name='default_paymentmethod' value='".$method->id."' class='btn btn-light' type='submit' title='".__( 'Favorite', 'doliconnect-pro' )."'><i class='far fa-star fa-1x fa-fw'></i></button>";
+} elseif ( (current_time( 'timestamp', 1) >= strtotime($method->expiration.'/1')) || ! preg_match('/pm_/', $method->id) ) {
+print "<button class='btn btn-light' title='".__( 'Favorite', 'doliconnect-pro' )."' disabled><i class='fas fa-ban fa-1x fa-fw'></i></button>";
 } else {
-print "<button class='btn btn-light' type='submit' title='".__( 'Favorite', 'doliconnect-pro' )."' disabled><i class='fas fa-ban fa-1x fa-fw'></i></button>";
+print "<button name='default_paymentmethod' value='".$method->id."' class='btn btn-light' type='submit' title='".__( 'Favorite', 'doliconnect-pro' )."'><i class='far fa-star fa-1x fa-fw'></i></button>";
 }
 print "<button name='delete_paymentmethod' value='".$method->id."' class='btn btn-light text-danger' type='submit' title='".__( 'Delete', 'doliconnect' )."'><i class='fas fa-trash fa-fw'></i></button>";
 print "</div></li>";
