c4920c792e50c92e45498007a6079bad26c9bc25
ptibogxiv <support@ptibogxiv.net>
2020-02-08 16:01:01 +0100

    Update doliconnect-pro.php

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -381,14 +381,6 @@ do_action( 'login_footer' );
 
 }
 
-// modal for CGU
-if (get_option('dolicgvcgu')){
-print "<div class='modal fade' id='cgvumention' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'><div class='modal-dialog modal-dialog-centered modal-lg' role='document'><div class='modal-content'><div class='modal-header'>
-<h5 class='modal-title' id='exampleModalLongTitle'>Conditions Generales d'Utilisation</h5>
-<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>
-<div class='modal-body'>
-en cours d'integration
-</div></div></div></div>";}
 }
 add_action( 'wp_footer', 'doliconnect_modal' );
 
