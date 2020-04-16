c33b41e78cc10563eb3d37b17d44720190dd89d8
ptibogxiv <support@ptibogxiv.net>
2020-04-16 20:31:21 +0200

    4.4.1

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: Doliconnect PRO
  * Plugin URI: https://www.ptibogxiv.net
  * Description: Premium Enhancement of Doliconnect
- * Version: 4.4.0
+ * Version: 4.4.1
  * Author: ptibogxiv
  * Author URI: https://www.ptibogxiv.net/en
  * Network: true
