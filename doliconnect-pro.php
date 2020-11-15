c6855368fdfe5675fd6bf6985948592163045756
ptibogxiv <support@ptibogxiv.net>
2020-11-15 15:41:22 +0100

    Update doliconnect-pro.php

--- doliconnect-pro.php
+++ doliconnect-pro.php
@@ -284,7 +284,7 @@ doliconnect_enqueues();
 
 do_action( 'login_head' );
 
-print "<div class='modal fade' id='DoliconnectLogin' tabindex='-1' role='dialog' aria-labelledby='DoliconnectLoginTitle' aria-hidden='true' data-backdrop='static' data-keyboard='false' ";
+print "<div class='modal fade' id='DoliconnectLogin' tabindex='-1' role='dialog' aria-labelledby='DoliconnectLoginTitle' aria-hidden='true' data-keyboard='false' ";
 
 print "><div class='modal-dialog modal-fullscreen-md-dow modal-dialog-centered modal-dialog-scrollable' role='document'><div class='modal-content'><div class='modal-header'>";
 
